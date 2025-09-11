9abeccead54d8bbea5cdea947ab4e96bf3c69ac5
NeverDecaf <jtr9289@gmail.com>
2025-09-10 21:21:54 -0400

    include global online times

--- operator_online.py
+++ operator_online.py
@@ -40,7 +40,39 @@ def scrape_PRTS():
         if len(pages) < limit:
             break
     return DATA
-
+def scrape_wiki():
+    # update the prts.wiki data with global release dates from arknights.wiki.gg/
+    # while it is possible to get limited status and some CN dates from the wiki, those only come from prts for now.
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+    }
+    url = "https://arknights.wiki.gg/api.php"
+    limit = 500
+    offset = 0
+    # if you need the rest of the data use fields "E.cn_date_start" and "E.event_type"
+    params = {
+        "action": "cargoquery",
+        "tables": "Operators=O,Events=E,OperatorFiles=F",
+        "fields": "E.global_date_start=start,F.id=charId",
+        "join_on": "O.event=E.name,F.name=O.name",
+        "format": "json",
+        "where": "F.id IS NOT NULL AND E.global_date_start IS NOT NULL",
+    }
+    while 1:
+        params['limit'] = limit
+        params['offset'] = offset
+        r = requests.get(url, params=params, headers=headers)
+        pages = r.json()['cargoquery']
+        for page in pages:
+            charId = page['title']['charId']
+            online = page['title']['start']
+            if charId in DATA:
+                DATA[charId]['onlineTime'] = online
+        offset += limit
+        if len(pages) < limit:
+            break
+    return DATA
 scrape_PRTS()
+scrape_wiki() # must call AFTER scrape_PRTS as it will update DATA (and relies on it being filled)
 with open('./json/operator_release_dates.json','w') as f:
     json.dump(DATA,f)
